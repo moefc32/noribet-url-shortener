b9fc4f4d2eed0ad44e465d96d7d6db53b30fc8a1
Faizal Chan. <moefc32@gmail.com>
2026-03-03 19:26:56 +0700

    add reserved paths

--- src/lib/isValidShortURL.js
+++ src/lib/isValidShortURL.js
@@ -1,4 +1,51 @@
+const reserved = [
+    '2fa',
+    'about',
+    'account',
+    'admin',
+    'administrator',
+    'api',
+    'assets',
+    'auth',
+    'billing',
+    'callback',
+    'confirm',
+    'contact',
+    'dashboard',
+    'docs',
+    'faq',
+    'health',
+    'help',
+    'home',
+    'index',
+    'init',
+    'legal',
+    'login',
+    'logout',
+    'me',
+    'metrics',
+    'mfa',
+    'privacy',
+    'profile',
+    'public',
+    'register',
+    'root',
+    'security',
+    'settings',
+    'sitemap',
+    'static',
+    'subscribe',
+    'support',
+    'terms',
+    'tos',
+    'verify',
+];
+
 export default function isValidShortURL(input) {
     if (input === '') return true;
-    return /^[A-Za-z0-9]+$/.test(input);
+
+    const isAlphanumeric = /^[A-Za-z0-9]+$/.test(input);
+    if (!isAlphanumeric) return false;
+
+    return !reserved.includes(input.toLowerCase());
 }
