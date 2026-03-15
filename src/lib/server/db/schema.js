9eaa1558503be94fbfc5521f42c285d390c01f71
Faizal Chan. <moefc32@gmail.com>
2026-03-15 23:35:43 +0700

    adjust db schema

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -2,28 +2,24 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 import { sql } from 'drizzle-orm';
 
 export const Users = sqliteTable('Users', {
-	id: text('id')
-		.primaryKey()
-		.default(sql`lower(hex(randomblob(16)))`),
+	id: text('id').primaryKey()
+		.$defaultFn(() => sql`lower(hex(randomblob(16)))`),
 	email: text('email').notNull().unique(),
 	password: text('password').notNull(),
 });
 
 export const Urls = sqliteTable('Urls', {
-	id: text('id')
-		.primaryKey()
-		.default(sql`lower(hex(randomblob(16)))`),
+	id: text('id').primaryKey()
+		.$defaultFn(() => sql`lower(hex(randomblob(16)))`),
 	short_url: text('short_url').notNull().unique(),
 	long_url: text('long_url').notNull(),
 	timestamp: integer('timestamp').notNull(),
 });
 
 export const Histories = sqliteTable('Histories', {
-	id: text('id')
-		.primaryKey()
-		.default(sql`lower(hex(randomblob(16)))`),
-	url_id: text('url_id')
-		.notNull()
+	id: text('id').primaryKey()
+		.$defaultFn(() => sql`lower(hex(randomblob(16)))`),
+	url_id: text('url_id').notNull()
 		.references(() => Urls.id, { onDelete: 'cascade' }),
 	ref: text('ref'),
 	agent: text('agent'),
